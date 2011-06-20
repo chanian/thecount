cb8c6588ba1a86a7198f663623f92f0cf5532ca0
Ian Chan <chanian@twitter.com>
2011-06-19 21:24:48 -0700

    updated misleading spec description

--- spec/thecount_spec.rb
+++ spec/thecount_spec.rb
@@ -5,7 +5,7 @@ require 'helper'
 describe TheCount do
   describe "#count" do
     context "given a single strategy input" do
-      it "should return an object with an key-value pair for the strategy type" do
+      it "should return an hash with an key-value pair for the strategy type" do
         counts = TheCount::count do |config|
           config[:strategies] = TheCount::Twitter
           config[:args] = { :url => 'http://google.com' }
@@ -15,7 +15,7 @@ describe TheCount do
       end
     end
     context "given multiple strategies as input" do
-      it "should return an array of strategies" do
+      it "should return an hash with an key-value pair for the strategy type" do
         counts = TheCount::count do |config|
           config[:strategies] = [TheCount::Facebook, TheCount::Twitter]
           config[:args] = { :url => 'http://google.com' }
